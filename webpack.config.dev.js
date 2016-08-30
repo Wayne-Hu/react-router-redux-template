57a6b93331013aee7cd7fa25a0919af4b6ec71c7
wayne1986 <huhuanfengwayne@163.com>
2016-08-31 11:53:44 +1200

    Add vendor.js and less-loader

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -1,26 +1,50 @@
 var webpack = require('webpack');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 module.exports = {
     output: {
         path: __dirname + '/build/js',
         filename: 'app.bundle.js'
     },
-    entry: [
-        'webpack-dev-server/client?http://localhost:3000',
-        'webpack/hot/only-dev-server',
-        __dirname + '/src/main.js'
-    ],
+    entry: {
+        index: [
+            'webpack-dev-server/client?http://localhost:3000',
+            'webpack/hot/dev-server',
+            __dirname + '/src/main.js'
+        ],
+        vendor: [
+            'react',
+            'react-dom',
+            'classnames',
+            'redux',
+            'react-redux',
+            'react-router',
+            'react-router-redux'
+        ]
+    },
     plugins: [
-        new webpack.HotModuleReplacementPlugin()
+        new webpack.HotModuleReplacementPlugin(),
+        new webpack.NoErrorsPlugin(),
+        new webpack.optimize.CommonsChunkPlugin('vendor', 'vendor.js'),
+        new ExtractTextPlugin('../css/styles.css')
     ],
     module: {
         loaders: [
             {
-                exclude: /(node_modules|bower_modules)/,
+                exclude: /(node_modules|bower_components)/,
                 test: /\.js?$/,
                 loaders: ['react-hot', 'babel-loader']
+            },
+            {
+                test: /\.less$/,
+                loader: ExtractTextPlugin.extract(
+                    // activate source maps via loader query
+                    'css?sourceMap!' +
+                    'less?sourceMap'
+                )
             }
         ]
     },
+    devtool: 'eval-source-map',
     watch: true
 };
--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -1,34 +1,49 @@
 var webpack = require('webpack');
-var HtmlWebPlugin = require('html-webpack-plugin');
 
 module.exports = {
     output: {
         path: __dirname + '/build/js',
-        filename: '[chunkhash:8].bundle.js'
+        filename: 'app.bundle.js'
     },
-    entry: __dirname + '/src/main.js',
+    entry: {
+        index: [
+            __dirname + '/src/main.js'
+        ],
+        vendor: [
+            'react',
+            'react-dom',
+            'classnames',
+            'redux',
+            'react-redux',
+            'react-router',
+            'react-router-redux'
+        ]
+    },
+    module: {
+        loaders: [
+            {
+                exclude: /(node_modules|bower_components)/,
+                test: /\.js?$/,
+                loaders: ['babel-loader']
+            },
+            {
+                test: /\.less$/,
+                loader: "style!css!less"
+            }
+        ]
+    },
+    devtool: 'eval',
     plugins: [
+        new webpack.optimize.CommonsChunkPlugin('vendor', 'vendor.js', Infinity),
         new webpack.optimize.UglifyJsPlugin({
             compress: {
                 warnings: false
             }
         }),
         new webpack.DefinePlugin({
-            'process.env.NODE_ENV': JSON.stringify('production')
-        }),
-        new HtmlWebPlugin({
-            filename: '../index.html',
-            title: 'Title',
-            inject: 'body'
-        })
-    ],
-    module: {
-        loaders: [
-            {
-                exclude: /(node_modules|bower_modules)/,
-                test: /\.js?$/,
-                loaders: ['babel-loader']
+            'process.env': {
+                'NODE_ENV': JSON.stringify('production')
             }
-        ]
-    }
+        })
+    ]
 };
